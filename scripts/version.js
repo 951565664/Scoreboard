abef38389963c97b933d9d510ee492881114afb5
““951565664” <951565664@qq.com>
2018-06-08 18:43:34 +0800

    修改docs

---
+++ scripts/version.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const beautify = require('js-beautify').js_beautify
+const config = require('./package.json')
+
+const writeVersion = () => new Promise((resolve, reject) => {
+  const { version } = config
+  const numbers = version.split('.')
+  numbers[2] = Number(numbers[2]) + 1
+  config.version = numbers.join('.')
+
+  fs.writeFile(path.join(__dirname, 'package.json'), beautify(JSON.stringify(config), { indent_size: 2 }), (err) => {
+    if (err) {
+      reject()
+    }
+    resolve()
+  })
+})
+
+
+
+const start = async () => {
+  await writeVersion()
+}
+
+start()
