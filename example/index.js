1b4d386df39f3a5494e8496a1e4c92d18ca600de
““951565664” <951565664@qq.com>
2018-06-08 19:02:55 +0800

    修改demo

--- example/index.js
+++ example/index.js
@@ -8,16 +8,23 @@ class App extends Component {
     state = {
         key: 2
     }
+    changeNum = (params) => {
+        this.setState({
+            key: parseInt(Math.random() * 10) + 2
+        })
+    }
+    componentDidMount = () => {
+        this.timer = setInterval(this.changeNum, 1000)
+    }
+    componentWillMount = () => {
+        clearInterval(this.timer)
+    }
+
     render() {
         let numberStr = new Array(this.state.key).fill(1).map((item) => parseInt(Math.random() * 10)).reduce((prev, curr, index, array) => '' + prev + curr);
         return (
             <div className={styles["wrapper"]}>
-                <h1>请点击下方的数字</h1>
-                <div onClick={() => {
-                    this.setState({
-                        key: parseInt(Math.random() * 10) + 2
-                    })
-                }} style={{ width: '100%' }}>
+                <div onClick={this.changeNum} style={{ width: '100%' }}>
                     <Scoreboard numberStr={numberStr} transitionDuration={'1s'} numberStyle={{ color: '#c40000', fontSize: '20px' }} />
                 </div>
             </div>
